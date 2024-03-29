a751b1c6c9d2344e3beb56bd9cafe30f8dfb979f
Marko <markojankovic2002@gmail.com>
2024-03-29 01:09:56 +0100

    better

--- Arrays/217ContainsDuplicate.cpp
+++ Arrays/217ContainsDuplicate.cpp
@@ -7,18 +7,13 @@ Given an integer array nums, return true if any value appears at least twice in 
 class Solution {
 public:
     bool containsDuplicate(std::vector<int>& nums) {
-        /* O(nlogn), space: O(1)
-        sort(nums.begin(),nums.end());
-        for(int i = 0; i < nums.size()-1;i++){
-            if(nums[i] == nums[i+1]) return true;
-        }
-        return false;
-    `   */  
+       
         std::unordered_map<int,int> hashset;
-        for(auto elem : nums) hashset[elem]++;
-
-        for(auto elem : hashset) 
-            if (elem.second >1 ) return true;
+        for(auto elem : nums){
+            hashset[elem]++;
+            if(hashset[elem] > 1) return true;
+        } 
         return  false;
+    }
     }
 };
